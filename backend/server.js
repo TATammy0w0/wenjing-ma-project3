f8d6b37c386bffca61078599f92672c05b662942
Tammy Ma <ma.wenjing1@northeastern.edu>
2024-12-10 01:52:08 -0800

    fixed typo in server.js and package.json

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,8 @@ app.use("/api/user", userRoutes);
 app.use("/api/posts", postRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/fontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
