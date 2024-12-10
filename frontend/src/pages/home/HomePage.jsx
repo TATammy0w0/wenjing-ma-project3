5d4cd2b699a9ee2efa667cc30ad0cf2b2d440c76
Tammy Ma <ma.wenjing1@northeastern.edu>
2024-12-10 00:24:41 -0800

    user able to create text only post, edit home page view base on logged in state

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import CreatePost from "../../components/common/CreatePost";
 import Posts from "../../components/common/Posts";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const HomePage = () => {
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+  const isLoggedIn = !!authUser;
+
   return (
     <>
       <div className="flex flex-col">
-        <CreatePost />
+        {isLoggedIn && <CreatePost />}
         <Posts />
       </div>
     </>
