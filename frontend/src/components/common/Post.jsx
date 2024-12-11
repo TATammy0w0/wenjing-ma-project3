83c174b42aa557cadc2c2244eafd7ba0599ec71f
Tammy Ma <ma.wenjing1@northeastern.edu>
2024-12-10 22:48:17 -0800

    edit post feature done

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -15,8 +15,6 @@ import LoadingSpinner from "./LoadingSpinner";
 import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
-  //const [comment, setComment] = useState("");
-  //const [editPost, setEditPost] = useState("");
   const { data: authUser } = useQuery({ queryKey: ["authUser"] });
   const queryClient = useQueryClient();
   const postOwner = post.user;
@@ -27,29 +25,14 @@ const Post = ({ post }) => {
   const [isEditingPost, setIsEditingPost] = useState(false);
   const [updatedText, setUpdatedText] = useState(post.text);
 
-  const [formData, setFormData] = useState({
-    text: "",
-  });
-
-  const handleInputChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
   const { mutate: deletePost, isPending: isDeleting } = useMutation({
     mutationFn: async () => {
-      try {
-        const res = await fetch(`/api/posts/${post._id}`, {
-          method: "DELETE",
-        });
+      const res = await fetch(`/api/posts/${post._id}`, {
+        method: "DELETE",
+      });
+      if (!res.ok) {
         const data = await res.json();
-
-        if (!res.ok) {
-          throw new Error(data.error || "Something went wrong");
-        }
-        return data;
-      } catch (error) {
-        toast.error(error.message);
-        throw error;
+        throw new Error(data.error || "Something went wrong");
       }
     },
     onSuccess: () => {
@@ -63,36 +46,30 @@ const Post = ({ post }) => {
   };
 
   const { mutate: updatePost, isPending: isUpdatingPost } = useMutation({
-    mutationFn: async (formData) => {
-      try {
-        const res = await fetch(`/api/posts/${post._id}`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        });
-
+    mutationFn: async (updatedData) => {
+      const res = await fetch(`/api/posts/${post._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedData),
+      });
+      if (!res.ok) {
         const data = await res.json();
-
-        if (!res.ok) {
-          throw new Error(data.error || "Something went wrong");
-        }
-        return data;
-      } catch (error) {
-        toast.error(error.message);
-        throw error;
+        throw new Error(data.error || "Something went wrong");
       }
+      return res.json();
     },
     onSuccess: () => {
       toast.success("Post edited successfully.");
       queryClient.invalidateQueries({ queryKey: ["posts"] });
+      setIsEditingPost(false); // Exit edit mode
     },
   });
 
-  const handleUpdatePost = () => {
-    e.preventDefault(); //prevent the default form submission behavior
-    updatePost(formData);
+  const handleUpdatePost = (e) => {
+    e.preventDefault();
+    updatePost({ newText: updatedText });
   };
 
   const handleEditClick = () => {
@@ -103,14 +80,6 @@ const Post = ({ post }) => {
     setIsEditingPost(false); // Exit edit mode
     setUpdatedText(post.text); // Reset the text
   };
-
-  //const isCommenting = false;
-
-  // const handlePostComment = (e) => {
-  //   e.preventDefault();
-  // };
-
-  //const handleLikePost = () => {};
 
   return (
     <>
@@ -167,9 +136,8 @@ const Post = ({ post }) => {
                 <textarea
                   placeholder="Update your post content."
                   className="flex-1 input border border-gray-700 rounded p-2 input-md"
-                  value={formData.text}
-                  name="text"
-                  onChange={handleInputChange}
+                  value={updatedText}
+                  onChange={(e) => setUpdatedText(e.target.value)}
                 />
                 <div className="flex flex-col gap-2">
                   <button
@@ -197,121 +165,12 @@ const Post = ({ post }) => {
             )}
           </div>
           <div className="flex justify-between mt-3">
-            <div className="flex gap-4 items-center w-2/3 justify-between">
-              {/* Comment post */}
-              <div
-                className="flex gap-1 items-center cursor-pointer group"
-                // onClick={() =>
-                //   document
-                //     .getElementById("comments_modal" + post._id)
-                //     .showModal()
-                // }
-              >
-                <FaRegComment className="w-4 h-4  text-slate-500 group-hover:text-sky-400" />
-                <span className="text-sm text-slate-500 group-hover:text-sky-400">
-                  0
-                </span>
-              </div>
-
-              {/* Comment Post with Modal Component from DaisyUI */}
-              {/* <dialog
-                id={`comments_modal${post._id}`}
-                className="modal border-none outline-none"
-              >
-                <div className="modal-box rounded border border-gray-600">
-                  <h3 className="font-bold text-lg mb-4">COMMENTS</h3>
-                  <div className="flex flex-col gap-3 max-h-60 overflow-auto">
-                    {post.comments.length === 0 && (
-                      <p className="text-sm text-slate-500">
-                        No comments yet 🤔 Be the first one 😉
-                      </p>
-                    )}
-                    {post.comments.map((comment) => (
-                      <div key={comment._id} className="flex gap-2 items-start">
-                        <div className="avatar">
-                          <div className="w-8 rounded-full">
-                            <img
-                              src={
-                                comment.user.profileImg ||
-                                "/avatar-placeholder.png"
-                              }
-                            />
-                          </div>
-                        </div>
-                        <div className="flex flex-col">
-                          <div className="flex items-center gap-1">
-                            <span className="font-bold">
-                              {comment.user.fullName}
-                            </span>
-                            <span className="text-gray-700 text-sm">
-                              @{comment.user.username}
-                            </span>
-                          </div>
-                          <div className="text-sm">{comment.text}</div>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                  <form
-                    className="flex gap-2 items-center mt-4 border-t border-gray-600 pt-2"
-                    onSubmit={handlePostComment}
-                  >
-                    <textarea
-                      className="textarea w-full p-1 rounded text-md resize-none border focus:outline-none  border-gray-800"
-                      placeholder="Add a comment..."
-                      value={comment}
-                      onChange={(e) => setComment(e.target.value)}
-                    />
-                    <button className="btn btn-primary rounded-full btn-sm text-white px-4">
-                      {isCommenting ? (
-                        <span className="loading loading-spinner loading-md"></span>
-                      ) : (
-                        "Post"
-                      )}
-                    </button>
-                  </form>
-                </div>
-                <form method="dialog" className="modal-backdrop">
-                  <button className="outline-none">close</button>
-                </form>
-              </dialog> */}
-
-              {/* Repost post */}
-              <div className="flex gap-1 items-center group cursor-pointer">
-                <BiRepost className="w-6 h-6  text-slate-500 group-hover:text-green-500" />
-                <span className="text-sm text-slate-500 group-hover:text-green-500">
-                  0
-                </span>
-              </div>
-
-              {/* Like post */}
-              <div
-                className="flex gap-1 items-center group cursor-pointer"
-                // onClick={handleLikePost}
-              >
-                {!isLiked && (
-                  <FaRegHeart className="w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500" />
-                )}
-                {isLiked && (
-                  <FaRegHeart className="w-4 h-4 cursor-pointer text-pink-500 " />
-                )}
-
-                <span
-                  className={`text-sm text-slate-500 group-hover:text-pink-500 ${
-                    isLiked ? "text-pink-500" : ""
-                  }`}
-                >
-                  0
-                </span>
-              </div>
-            </div>
-            <div className="flex w-1/3 justify-end gap-2 items-center">
-              <FaRegBookmark className="w-4 h-4 text-slate-500 cursor-pointer" />
-            </div>
+            {/* Buttons for comment, like, etc. */}
           </div>
         </div>
       </div>
     </>
   );
 };
+
 export default Post;
